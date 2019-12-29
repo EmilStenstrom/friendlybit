d843d85eb6b40848292e519c7875ac151938cc68
Emil Stenström <em@kth.se>
2019-12-29 10:47:50 +0100

    Script to strip unused frontmatter.

---
+++ commands/remove_unused_frontmatter.py
@@ -0,0 +1,33 @@
+from glob import glob
+
+import frontmatter
+
+WHITELISTED_METADATA = [
+    "id",
+    "title",
+    "date",
+    "author",
+    "guid",
+    "permalink",
+    "categories",
+]
+
+def main():
+    for filename in sorted(glob("posts/*.md"), reverse=True):
+        with open(filename, "r") as f:
+            comment_post = frontmatter.load(f)
+
+        new_metadata = {}
+        for field, data in comment_post.metadata.items():
+            if field not in WHITELISTED_METADATA:
+                continue
+
+            new_metadata[field] = data
+
+        with open(filename, "wb") as f:
+            new_comment_post = frontmatter.Post(comment_post.content, **new_metadata)
+            frontmatter.dump(new_comment_post, f)
+
+
+if __name__ == '__main__':
+    main()
