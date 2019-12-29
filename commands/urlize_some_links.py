cfa1a7f23fc4bfdea3881bd6c1220da0215d8d23
Emil Stenström <em@kth.se>
2019-12-29 15:48:52 +0100

    Add script to link unlinked urls in comments.

---
+++ commands/urlize_some_links.py
@@ -0,0 +1,32 @@
+import re
+from glob import glob
+
+import frontmatter
+
+URL_NOT_ALREADY_URLIZED = re.compile(r'(?<!")(https?://[^ ]+)(?!")')
+
+def main():
+    for filename in sorted(glob("comments/*.md"), reverse=True):
+        with open(filename, "r") as f:
+            comment_post = frontmatter.load(f)
+
+        comments = comment_post.metadata["comments"]
+        new_comments = []
+        for comment in comments:
+            content = comment["comment_content"]
+            if re.search(URL_NOT_ALREADY_URLIZED, content):
+                comment["comment_content"] = re.sub(
+                    URL_NOT_ALREADY_URLIZED,
+                    '<a href="\\1" rel="nofollow">\\1</a>',
+                    content
+                )
+
+            new_comments.append(comment)
+
+        with open(filename, "wb") as f:
+            comment_post.metadata["comments"] = new_comments
+            frontmatter.dump(comment_post, f)
+
+
+if __name__ == '__main__':
+    main()
