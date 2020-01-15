b1d6c9d6a558f7d34dcf70133289154ffd531efe
Emil Stenström <em@kth.se>
2020-01-15 08:16:37 +0100

    Name all routes.

--- friendlybit/server.py
+++ friendlybit/server.py
@@ -5,15 +5,15 @@ from starlette.staticfiles import StaticFiles
 from friendlybit.views import homepage, favicon, css, feed, contact, post
 
 routes = [
-    Route("/", endpoint=homepage),
-    Route("/articles/{category}/", endpoint=homepage),
-    Route("/favicon.ico", endpoint=favicon),
-    Route("/style.css", endpoint=css),
-    Route("/feed/", endpoint=feed),
-    Route("/feed/{category}/", endpoint=feed),
-    Route("/contact/", endpoint=contact),
+    Route("/", endpoint=homepage, name="homepage"),
+    Route("/articles/{category}/", endpoint=homepage, name="category"),
+    Route("/favicon.ico", endpoint=favicon, name="favicon"),
+    Route("/style.css", endpoint=css, name="css"),
+    Route("/feed/", endpoint=feed, name="feed"),
+    Route("/feed/{category}/", endpoint=feed, name="feed_category"),
+    Route("/contact/", endpoint=contact, name="contact"),
     Mount("/files", app=StaticFiles(directory='files', html=True), name="static"),
-    Route("/{category}/{slug}/", endpoint=post),
+    Route("/{category}/{slug}/", endpoint=post, name="post"),
 ]
 
 app = Starlette(debug=True, routes=routes)
